b4322228a9c9d62f715141a594007cf3b794583b
upteran <upteran@gmail.com>
2022-07-17 21:30:04 +0300

    upd: messages styles and view logic

--- src/client/screen/Chat/MessagesView/index.tsx
+++ src/client/screen/Chat/MessagesView/index.tsx
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useStore } from "effector-react";
+import cx from "classnames";
 
 import { Message } from "entity/messages/types";
 import { $messagesList } from "entity/messages/store";
 import { $chatStore } from "entity/chat/store";
+import { $userStore } from "entity/user/store";
 
 import "./styles.css";
 
 export const MessagesView = () => {
+  const cont = useRef(null);
   const messages = useStore($messagesList);
   const { messagesInfoMap } = useStore($chatStore);
+  const { name } = useStore($userStore);
+  const userMap = messagesInfoMap || {};
+  useEffect(() => {
+    if (cont?.current !== null) {
+      // @ts-ignore
+      cont.current.scrollTop = cont.current.scrollHeight;
+    }
+  }, [messages.length]);
   return (
-    <div className="messages">
+    <div className="messages" ref={cont}>
       {messages.map(({ message, messageId, userId }: Message) => (
         <div
           key={messageId}
-          style={{
-            color: `${
-              messagesInfoMap ? messagesInfoMap[userId].color : "#000"
-            }`,
-          }}
+          className={cx("message", {
+            mainUser: name === userMap[userId].name,
+          })}
         >
-          [{new Date().toLocaleDateString()}][
-          {messagesInfoMap ? messagesInfoMap[userId].name : "name"}]:{message}
+          <div className="messageUserName">
+            {userMap[userId]?.name || "name"}:{" "}
+            <span>{new Date().toLocaleDateString()}</span>
+          </div>
+          {message}
         </div>
       ))}
     </div>
